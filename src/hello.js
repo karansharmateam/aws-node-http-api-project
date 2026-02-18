df75ccf4c3522588bbb9be5861713a79866b5d2e
codekaran <karansharma2820@gmail.com>
2026-02-18 15:33:34 +0530

    Update message in hello.js for final testing

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From TWS Batch 5 - You all are awesome!",
+        message: "Final testing of the project it show on ci/cd",
       },
       null,
       2
